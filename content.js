ef89da15549aa54b892eb4bb6e88b26c50f136b0
Tuệ Hồ <tueho>
2023-03-19 18:25:30 +0700

    oke

---
+++ content.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", (event) => {
+  console.log("page is fully loaded");
+
+  let header = document.querySelector("h1");
+  header.textContent = "Hello, world!";
+});
---
+++ js/index.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", (event) => {
+  let header = document.querySelector(".game-log-other");
+  console.log("oke");
+});
